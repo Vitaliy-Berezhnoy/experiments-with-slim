c74384284032597bd7a71366f8d266cbf6bad7da
Виталий <ipbvv@mail.ru>
2025-11-05 20:29:27 +0300

    Finishes the lesson Template Engine

--- public/index.php
+++ public/index.php
@@ -3,15 +3,23 @@
 // Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
+// Контейнеры в этом курсе не рассматриваются (это тема связанная с самим ООП), но если интересно, то посмотрите DI Container
 use Slim\Factory\AppFactory;
+use DI\Container;
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
+//  $app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
 
 $users = [];
 for ($i = 1; $i < 11; $i++) {
     $users[] = "Name{$i}";
 };
-
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
@@ -39,6 +47,15 @@ $app->get('/users', function ($request, $response) {
 $app->get('/courses/{id}', function ($request, $response, $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
+});
+
+$app->get("/users/{id}", function ($request, $response, $args) {
+    $id = $args["id"];
+    $params =["id" => $id, "nickname" => "User - " . $id];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
